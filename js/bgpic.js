1d06d178bc4aec8ad2a54e70a97c5f9d584b87c4
1208nn <74806550+1208nn@users.noreply.github.com>
2022-02-26 16:26:18 +0800

    1

--- js/bgpic.js
+++ js/bgpic.js
@@ -1,6 +1,6 @@
 var timeInterval = 10000;
 setInterval(changeImg, timeInterval);
 function changeImg() {
-  var curIndex Math.floor(Math.random() * 5);
+  var curIndex=Math.floor(Math.random() * 5);
   document.getElementById("bgpic").style.backgroundImage = "URL(./backgroung/"+curIndex+".jpg)";
 }
