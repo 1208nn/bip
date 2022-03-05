50ecc1289f9bbbc4550af9b39dcbcdd950cb2e5c
1208nn <74806550+1208nn@users.noreply.github.com>
2022-03-05 18:23:22 +0800

    1

--- js/nav.js
+++ js/nav.js
@@ -36,6 +36,6 @@ function getElementViewTop(element){
     window.addEventListener('scroll',function(event){ 
         var scrollTop = getScroll().top; 
         if(posTop>=30 && posTop-scrollTop <= 30)  
-            nav.className = 'nav fixed'; 
+            nav.className = 'nav_fixed'; 
         else nav.className = 'nav'; 
     },false); 
